6d86ae67eb9fba3cfe7ddbda8e9a10d090705426
robertito1 <orazurobert@yahoo.com>
2020-08-30 01:03:22 +0100

    completed exercise 2.12

--- PART_2/countries/src/App.js
+++ PART_2/countries/src/App.js
@@ -1,41 +1,39 @@
 import React, { useState, useEffect } from 'react';
-import Axios from 'axios'
+import axios from 'axios'
 import './App.css';
 
 
 function App() {
 
-  const [names, setNames] = useState('')
-  const [toShow, setToShow] = useState(false)
+  const [names, setNames] = useState([])
+  const [toShow, setToShow] = useState('')
 
   useEffect(() => {
     console.log('effect')
-    Axios
+    axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
         console.log('promise fulfilled')
-        setNames(response.data.map(country => <p>{country.name}</p>))
+        setNames(response.data)
       })
   }, [])
 
-  const filterConturies = (e) => {
-    const input = e.target.value
-    if (input) {
-      setToShow(true)
-    }
-    return input
+  const filterContries = (e) => {
+    setToShow(e.target.value.toLowerCase())
+
   }
 
-  // const filtered = toShow ? null : names.filter(nation => nation.name.toLowerCase().includes(input))
+  const filtered = toShow === 0 ? 'search' : names.filter(nation => nation.name.toLowerCase().includes(toShow))
 
   const header = 'welcome'
   return (
     <div className="App">
       <h1>{header}</h1>
+      <input onChange={filterContries} value={toShow} />
       <div>{
-        names
+        filtered.length > 10 ? 'too long' : filtered.map(country => <div>{country.name}<button>show</button></div>)
       }</div>
-      <input onChange={filterConturies} />
+
     </div>
   );
 }
