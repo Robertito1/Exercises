b1c19fac92fa7ac14fe2553817795cac8cb9b253
robertito1 <orazurobert@yahoo.com>
2020-08-31 01:22:30 +0100

    still on part 2

--- PART_2/countries/src/App.js
+++ PART_2/countries/src/App.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
-import './App.css';
 
-
+const FilteredCountries = ({ onInputChange, toShow, filtered }) => {
+  return (
+    <div>
+      <input onChange={onInputChange} value={toShow} />
+      <div>{
+        filtered
+      }</div>
+    </div>
+  )
+}
 function App() {
 
   const [names, setNames] = useState([])
   const [toShow, setToShow] = useState('')
+  const [displayCountryDetails, setDisplayCountryDetails] = useState(false)
 
   useEffect(() => {
     console.log('effect')
@@ -18,22 +27,46 @@ function App() {
       })
   }, [])
 
-  const filterContries = (e) => {
-    setToShow(e.target.value.toLowerCase())
-
+  const showCountryDetails = (e) => {
+    setDisplayCountryDetails(!displayCountryDetails)
+    console.log(e.target.parentNode)
   }
 
-  const filtered = toShow === 0 ? 'search' : names.filter(nation => nation.name.toLowerCase().includes(toShow))
+  const filterCountries = (e) => {
+    setToShow(e.target.value.toLowerCase())
+  }
+
+  let filtered = toShow === '' ? 'search' : names.filter(nation => nation.name.toLowerCase().includes(toShow))
+
+  const countryDetail = !displayCountryDetails ? '' : <div>Country Details</div>
+
+  if (filtered === 'search') {
+    filtered = 'search'
+  } else if (filtered.length > 10) {
+    filtered = 'search more'
+  } else if (filtered.length < 10 && filtered.length > 2) {
+    filtered = filtered.map(country =>
+      <div id={country.name} key={country.name}>
+        {countryDetail}
+        {country.name}<button id={country.name} onClick={showCountryDetails}>show</button>
+      </div>)
+  } else {
+    filtered = filtered.map(country =>
+      <div key={country.name}>
+        <h1>{country.name}</h1>
+        <p>capital {country.capital}</p>
+        <p>population {country.population}</p>
+        <h3>languages</h3>
+        <ul>{country.languages.map(language => <li>{language.name}</li>)}</ul>
+        <img src={country.flag} alt='flag' />
+      </div>)
+  }
 
   const header = 'welcome'
   return (
     <div className="App">
       <h1>{header}</h1>
-      <input onChange={filterContries} value={toShow} />
-      <div>{
-        filtered.length > 10 ? 'too long' : filtered.map(country => <div>{country.name}<button>show</button></div>)
-      }</div>
-
+      <FilteredCountries onInputChange={filterCountries} toShow={toShow} filtered={filtered} />
     </div>
   );
 }
