c3a7b90b8df02be4376763d417ee6f1abac71173
robertito1 <orazurobert@yahoo.com>
2020-10-09 15:49:59 +0100

    part 5.12

--- PART_5/bloglist/frontend/src/components/Blog.js
+++ PART_5/bloglist/frontend/src/components/Blog.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import blogService from "../services/blogs";
+import PropTypes from "prop-types";
 
 const Blog = ({ blog, handleDelete, user }) => {
   const [expand, setExpand] = useState(false);
@@ -69,6 +70,11 @@ const Blog = ({ blog, handleDelete, user }) => {
       )}
     </div>
   );
+};
+Blog.propTypes = {
+  blog: PropTypes.object.isRequired,
+  handleDelete: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
 };
 
 export default Blog;
