8d1d09a5b3e09cf83f8cd15fc897737205a9543a
robertito1 <orazurobert@yahoo.com>
2020-10-20 13:34:48 +0100

    part 6.8 done

--- PART_6/redux-anecdotes/src/App.js
+++ PART_6/redux-anecdotes/src/App.js
@@ -3,11 +3,6 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-   
-
-
-
-
   return (
     <div>
     <AnecdoteList/>
