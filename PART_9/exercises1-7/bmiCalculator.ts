0441d9f72e3e65ca23e29d8a27dcb495ebc4ef37
robertito1 <orazurobert@yahoo.com>
2020-12-30 11:14:41 +0000

    part 9.3 done

--- PART_9/exercises1-7/bmiCalculator.ts
+++ PART_9/exercises1-7/bmiCalculator.ts
@@ -1,4 +1,20 @@
+interface BmiValues {
+  value1: number;
+  value2: number;
+}
+let parseArguments = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
 
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
 
 let calculateBmi = (height: number, weight: number) : string => {
   let bmi =   weight / Math.pow((height / 100),(2))
@@ -13,4 +29,9 @@ let calculateBmi = (height: number, weight: number) : string => {
   }
 }
 
-console.log(calculateBmi(180, 74))
+try {
+  const { value1, value2 } = parseArguments(process.argv);
+  console.log(calculateBmi(value1, value2));
+} catch (e) {
+  console.log('Error, something bad happened, message: ', e.message);
+}
--- PART_9/exercises1-7/exerciseCalculator.ts
+++ PART_9/exercises1-7/exerciseCalculator.ts
@@ -1,3 +1,9 @@
+   export {};
+
+   interface ExerciseValues {
+       days: Array<number>;
+       target: number
+   }
    interface Result  {
      totalDays: number;
      trainingDays: number;
@@ -40,16 +46,34 @@
            this.totalDays = data.length;
            this.trainingDays = data.filter(e => e !== 0).length;
            this.targetAverage = target;
-           this.achievedAverage = data.reduce((a, c ) => a + c) / 7;
+           this.achievedAverage = data.reduce((a, c ) => a + c) / this.totalDays;
            this.success = this.achievedAverage >= target ? true : false;
            this.rating = this.calcRating(this.achievedAverage, target);
            this.ratingDescription = this.getFeedback(this.rating);
         }     
     }
 
+    let parseArguments = (args: Array<string>): ExerciseValues => {
+        if (args.length < 4) throw new Error('Not enough arguments');
+      
+        if (args.slice(2).find(e => isNaN(Number(e)))) {
+         throw new Error('Provided values were not all numbers!');
+        } else {
+            return {
+                days: args.slice(2, args.length - 1).map(e => Number(e)),
+                target: Number(args[args.length - 1])
+              }
+        }
+      }
+
   let calculateExercise = (data: Array<number>, target: number) : Result => {
    let output =  new ResultObject(data,target)
      const {calcRating, getFeedback, ...rest} = output
      return rest
   }
-  console.log(calculateExercise([1,0,2.5,4,0.4,3.1,2], 2))
+  try {
+    const { days, target } = parseArguments(process.argv);
+    console.log(calculateExercise(days, target));
+  } catch (e) {
+    console.log('Error, something bad happened, message: ', e.message);
+  }
